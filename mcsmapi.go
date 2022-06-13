a608f05a35f8f2eb0d4fce2811a3ce6bac29018a
zijiren <pyh1670605849@gmail.com>
2022-06-13 13:02:54 +0800

    update readme

--- mcsmapi.go
+++ mcsmapi.go
@@ -92,7 +92,7 @@ func GetMConfig() MConfig {
 			"remote_uuid": "d6a27b0b13ad44ce879b5a56c88b4d34", // 守护进程的GID （守护进程标识符）
 			"uuid": "a8788991a64e4a06b76d539b35db1b16", // 实例的UID （远程/本地实例标识符）
 			"apikey": "vmajkfnvklNSdvkjbnfkdsnv7e0f", // 不可为空，用户中心->右上角个人资料->右方生成API密钥
-			"group_id": "234532", // 要管理的QQ群号，如果多个实例要监听同一个群，那么下面的群管理员列表应该设置相同
+			"group_id": "234532", // 要管理的QQ群号
 			"adminlist": [
 				"1145141919", // 群管理员，第一个为主管理员，只有管理员才可以发送命令
 				"1145141919" // 管理员列表可以为空，则所有用户都可以发送命令
@@ -105,9 +105,9 @@ func GetMConfig() MConfig {
 			"remote_uuid": "d6a27b0b13ad44ce879b5ascwfscr323",
 			"uuid": "a8788991a6acasfaca76d539b35db1b16",
 			"apikey": "6ewc6292daefvlksmdvjadnvjbf",
-			"group_id": "234532", // 多个实例监听同一个群，下面的管理员列表应设置一样
+			"group_id": "234532",
 			"adminlist": [
-				"1145141919", // 多个实例监听同一个群，管理员列表应设置一样
+				"1145141919",
 				"1145141919"
 			]
 		} // <--最后一个实例配置这里没有逗号！！！
